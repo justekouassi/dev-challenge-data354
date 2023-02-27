883b7d0f6c141898a5a412f843e3d9ef36a06831
Juste Kouassi <kjuste02@gmail.com>
2023-02-27 12:39:49 +0000

    démarrage projet

---
+++ app.py
@@ -0,0 +1,39 @@
+'''ghh'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    ''' représente la page principale de notre application
+	qui affiche la visualisation des données prises l'un des
+    capteurs en fonction d'une période donnée
+	'''
+    return render_template("index.html", title="Accueil")
+
+
+@app.route('/login')
+def login() -> str:
+    ''' assure la connexion d'un utilisateur
+    '''
+    return render_template("login.html", title="Connexion")
+
+
+@app.route('/signup')
+def signup() -> str:
+    ''' assure l'inscription d'un utilisateur
+    '''
+    return render_template("signup.html", title="Inscription")
+
+
+@app.route('/prediction')
+def prediction() -> str:
+    ''' assure la prédiction
+    '''
+    return render_template("prediction.html", title="Prédiction")
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host='127.0.0.1', port='4000', debug=True)
